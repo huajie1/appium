ee6106eeeffa4f1f6fd1e14ffa1c777e49541cd7
huajie1 <limaohuaym@126.com>
2015-09-15 20:40:28 +0800

    README.md更新

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# appium
+﻿# appium
 ##appium基础学习工程  
 1.appium-Java环境配置（新建工程添加java-client依赖)<br>
 2.设置机型设置属性<br>
 3.设置被测应用包名及启动activity：使用命令`aapt dump badging` <包路径>可以查看  
 4.实例化session  
 5.可以开始编写UI测试脚本啦  
-6.本工程中实现了'byXPath','byAndroidUIAutomator','byClassName'三种定位元素方式，详见代码  
+6.本工程中定位方法类实现了'byXPath','byAndroidUIAutomator','byClassName'三种定位元素方式，详见代码；且用另一个app实现了HybridApp的测试实践；MobileWebApp的测试实践也已添加
 tips:  
 1.如果不想每执行一个test都重装一次应用这样设置即可
 ```
--- src/test/java/com/appium/jinritoutiao/LocateMethodsTest.java
+++ src/test/java/com/appium/jinritoutiao/LocateMethodsTest.java
@@ -38,25 +38,25 @@ public class LocateMethodsTest {
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
 	}
-//    //byXPath
-//    @Test
-//    public void byXPath() {
-//    	
-//    	driver.findElementByXPath("//android.widget.ListView/android.widget.FrameLayout[1]").click();//偶尔点到的其实不是第一个，所以用uiautomator会更靠谱
-//    	driver.findElementByXPath("//android.widget.TextView[contains(@text,'写评论')]").click();
-//    	WebElement title = driver.findElementByXPath("//android.widget.TextView[contains(@text,'登录今日头条')]");
-//    	title.isDisplayed();
-//    }
-//    //byAndroidUIAutomator
-//    @Test
-//    public void byAndroidUIAutomatorTest(){
-//        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/category_text\").text(\"热点\")").click();//进入热点
-//        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.ss.android.article.news:id/view_pager\")).scrollIntoView(new UiSelector().resourceId(\"com.ss.android.article.news:id/title\").text(\"全球最大婚外情网站被黑\"))").click();
-//	    driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/action_view_comment\")").click();//点击评论
-//	    WebElement hotTopic = driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/section_text\").text(\"热门评论\")");
-//	    hotTopic.isDisplayed();//验证界面上有热门评论模块即为成功
-//	    
-//    }
+    //byXPath
+    @Test
+    public void byXPath() {
+    	
+    	driver.findElementByXPath("//android.widget.ListView/android.widget.FrameLayout[1]").click();//偶尔点到的其实不是第一个，所以用uiautomator会更靠谱
+    	driver.findElementByXPath("//android.widget.TextView[contains(@text,'写评论')]").click();
+    	WebElement title = driver.findElementByXPath("//android.widget.TextView[contains(@text,'登录今日头条')]");
+    	title.isDisplayed();
+    }
+    //byAndroidUIAutomator
+    @Test
+    public void byAndroidUIAutomatorTest(){
+        driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/category_text\").text(\"热点\")").click();//进入热点
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().resourceId(\"com.ss.android.article.news:id/view_pager\")).scrollIntoView(new UiSelector().resourceId(\"com.ss.android.article.news:id/title\").text(\"全球最大婚外情网站被黑\"))").click();
+	    driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/action_view_comment\")").click();//点击评论
+	    WebElement hotTopic = driver.findElementByAndroidUIAutomator("new UiSelector().resourceId(\"com.ss.android.article.news:id/section_text\").text(\"热门评论\")");
+	    hotTopic.isDisplayed();//验证界面上有热门评论模块即为成功
+	    
+    }
     //byClassName进入今日头条->个人信息->更多登录方式->注册新账号  的流程
     @Test
     public void byClassNameTest() throws InterruptedException{
